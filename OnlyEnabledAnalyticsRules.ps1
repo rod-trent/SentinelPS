5a0467d9a0d94420f2d03134fb2e5545b3521f8c
Rod Trent <56966432+rod-trent@users.noreply.github.com>
2023-02-21 12:43:53 -0500

    Create OnlyEnabledAnalyticsRules.ps1

---
+++ OnlyEnabledAnalyticsRules.ps1
@@ -0,0 +1,14 @@
+//PowerShell script that connects to Microsoft Sentinel and displays only the enabled Microsoft Sentinel Analytics Rules
+//Replace <TenantId> and <SubscriptionId> with your actual values.
+
+# Connect to Microsoft Sentinel
+Connect-AzSentinel -TenantId <TenantId> -SubscriptionId <SubscriptionId>
+
+# Get all analytics rules
+$rules = Get-AzSentinelAnalyticsRule
+
+# Filter only enabled rules
+$enabledRules = $rules | Where-Object { $_.Enabled -eq $true }
+
+# Display only the enabled rules
+$enabledRules | Select-Object DisplayName, Description, Enabled
