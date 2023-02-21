ca4d49061e1cc36d9a8f520841e8ac06127e0ee9
Rod Trent <56966432+rod-trent@users.noreply.github.com>
2023-02-21 17:58:26 -0500

    Update ExportSentinelWatchlist.ps1

--- ExportSentinelWatchlist.ps1
+++ ExportSentinelWatchlist.ps1
@@ -1,6 +1,6 @@
-//Exports a Microsoft Sentinel Watchlist
+# Exports a Microsoft Sentinel Watchlist
 
-//This script uses the Connect-AzSentinel cmdlet to connect to the Microsoft Sentinel workspace, the Get-AzSentinelWatchlist cmdlet to get the watchlist, and the Export-Csv cmdlet to export the watchlist to a CSV file. Replace <tenantId>, <subscriptionId>, <workspaceId>, <watchlistName>, and <filePath> with the appropriate values for your environment.
+# This script uses the Connect-AzSentinel cmdlet to connect to the Microsoft Sentinel workspace, the Get-AzSentinelWatchlist cmdlet to get the watchlist, and the Export-Csv cmdlet to export the watchlist to a CSV file. Replace <tenantId>, <subscriptionId>, <workspaceId>, <watchlistName>, and <filePath> with the appropriate values for your environment.
 
 # Connect to the Microsoft Sentinel workspace
 Connect-AzSentinel -TenantId <tenantId> -SubscriptionId <subscriptionId> -WorkspaceId <workspaceId>
